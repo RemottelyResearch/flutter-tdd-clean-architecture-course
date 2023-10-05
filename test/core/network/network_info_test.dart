5b5682c3fa4ac9a48425a511f2da09c62aa90816
KevinKobori <kevin.kobori@hotmail.com>
2023-10-05 16:49:59 -0300

    Fixed packages dont support null safety

--- test/core/network/network_info_test.dart
+++ test/core/network/network_info_test.dart
@@ -1,7 +1,7 @@
 import 'package:clean_architecture_tdd_course/core/network/network_info.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:data_connection_checker/data_connection_checker.dart';
+// import 'package:data_connection_checker/data_connection_checker.dart';
 
 class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
 
